9292707d2ff8659d3fe9ac54c3d76aeac3a0fd8a
RenardBlanc <baktacheilyas@gmail.com>
2022-11-28 14:51:22 -0500

    gan debug

--- GAN/train_gan.py
+++ GAN/train_gan.py
@@ -364,31 +364,30 @@ class model():
         return discriminateur
 
     def generateur(nb_coord,latent_dim,nb_class):
-        if nb_coord%4 == 0 :
-            generateur = Sequential()
-            #Head 1
-            in_lat = Input(shape = (latent_dim,))
-            dense1 = Dense(32*nb_coord, activation='relu')(in_lat)
-            fct1 = LeakyReLU(alpha = 0.2)(dense1)
-            resh1 = Reshape((int(nb_coord/4),128))(fct1)
-            #Head 2
-            in_lab = Input(shape = (1,))
-            emb2 = Embedding(nb_class,50)(in_lab)
-            dense2 = Dense(50, activation='relu')(emb2)
-            resh2 = Reshape((int(nb_coord/4),1))(dense2)
 
-            # merge
-            conc1 = concatenate([resh1, resh2])
-            convt1 = Conv1DTranspose(filters=128, kernel_size=51)(conc1)
-            fct2 = LeakyReLU(alpha = 0.2)(convt1)
-            convt2 = Conv1DTranspose(filters=128, kernel_size=101)(fct2)
-            fct3 = LeakyReLU(alpha = 0.2)(convt2)
-            output = Conv1D(filters=1, kernel_size=1,activation='tanh', padding='same')(fct3)
+        generateur = Sequential()
+        #Head 1
+        in_lat = Input(shape = (latent_dim,))
+        dense1 = Dense(32*nb_coord, activation='relu')(in_lat)
+        fct1 = LeakyReLU(alpha = 0.2)(dense1)
+        resh1 = Reshape((int(nb_coord/4),128))(fct1)
+        #Head 2
+        in_lab = Input(shape = (1,))
+        emb2 = Embedding(nb_class,50)(in_lab)
+        dense2 = Dense(50, activation='relu')(emb2)
+        resh2 = Reshape((int(nb_coord/4),1))(dense2)
 
-            generateur = Model(inputs = [in_lat,in_lab],outputs = output)
-            return generateur
-        else :
-            error
+        # merge
+        conc1 = concatenate([resh1, resh2])
+        convt1 = Conv1DTranspose(filters=128, kernel_size=51)(conc1)
+        fct2 = LeakyReLU(alpha = 0.2)(convt1)
+        convt2 = Conv1DTranspose(filters=128, kernel_size=101)(fct2)
+        fct3 = LeakyReLU(alpha = 0.2)(convt2)
+        output = Conv1D(filters=1, kernel_size=1,activation='tanh', padding='same')(fct3)
+
+        generateur = Model(inputs = [in_lat,in_lab],outputs = output)
+        return generateur
+
     
     # use the generator to generate n fake examples, with class labels
     def generate_fake_samples(generator, latent_dim, n_samples):
