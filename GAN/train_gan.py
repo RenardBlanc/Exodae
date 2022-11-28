634b7000af9568fdee9dc73a7615fa788576b2cf
RenardBlanc <baktacheilyas@gmail.com>
2022-11-28 14:49:42 -0500

    gan debug

--- GAN/train_gan.py
+++ GAN/train_gan.py
@@ -359,7 +359,7 @@ class model():
 
         discriminateur = Model([in_coord, in_label], output)
         # compile model
-        opt = Adam(lr=0.0002, beta_1=0.5)
+        opt = Adam(learning_rate=0.0002, beta_1=0.5)
         discriminateur.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
         return discriminateur
 
@@ -405,15 +405,15 @@ class model():
         # make weights in the discriminator not trainable
         d_model.trainable = False
         # get noise and label inputs from generator model
-        gen_noise, gen_label = g_model.inputs
+        gen_noise, gen_label = g_model.input
         # get coord output from the generator model
-        gen_output = g_model.outputs
+        gen_output = g_model.output
         # connect image output and label input from generator as inputs to discriminator
         gan_output = d_model([gen_output, gen_label])
         # define gan model as taking noise and label and outputting a classification
         model = Model([gen_noise, gen_label], gan_output)
         # compile model
-        opt = Adam(lr=0.0002, beta_1=0.5)
+        opt = Adam(learning_rate=0.0002, beta_1=0.5)
         model.compile(loss='binary_crossentropy', optimizer=opt)
         return model
         
