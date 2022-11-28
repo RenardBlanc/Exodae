8e464c9d46af24d6f8b83a3871348bc8f9322369
RenardBlanc <baktacheilyas@gmail.com>
2022-11-28 15:32:44 -0500

    gan debug

--- GAN/train_gan.py
+++ GAN/train_gan.py
@@ -316,8 +316,9 @@ class pre_process_GAN:
         return x_train,y_train,nb_class,x_coord_ini
 
     def generate_real_samples(x_train,y_train,n_samples):
+        print(x_train.shape[1])
         # choose random instances
-        ix = random.randint(0, x_train.shape[0], n_samples)
+        ix = random.randint(0, x_train.shape[1], n_samples)
         # select images and labels
         X, labels = x_train[ix], y_train[ix]
         # generate class labels
