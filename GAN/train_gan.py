af6869a47c26631c5b4c7db790ccb2d63f74f59f
RenardBlanc <baktacheilyas@gmail.com>
2022-12-07 16:22:00 -0500

    debug gan

--- GAN/train_gan.py
+++ GAN/train_gan.py
@@ -313,18 +313,18 @@ class pre_process_GAN:
         y_train = np.array(dict_ok['y_train']).astype('int')
         nb_class =  np.array(dict_ok['nb_classe'])
         x_coord_ini = np.array(dict_ok['coordonnee_x']).astype('float32')
-        return x_train,y_train,nb_class,x_coord_ini
+        return x_train.T,y_train,nb_class,x_coord_ini
 
     def generate_real_samples(x_train,y_train,n_samples):
         nb_profil = x_train.shape[1]
         # choose random instances
         ix = np.random.randint(0, nb_profil, n_samples)
         # select images and labels
-        X, labels = x_train[:,ix], y_train[ix]
+        X, labels = x_train[ix,:], y_train[ix]
         # generate class labels
         y = np.ones((n_samples, 1))
         print(np.shape(X),np.shape(labels))
-        return [X, labels.T], y
+        return [X, labels], y
     
     # generate points in latent space as input for the generator
     def generate_latent_points(latent_dim, n_samples, n_classes=10):
@@ -423,8 +423,8 @@ class model():
     def train_model(Mach,Re,x_train,y_train,latent_dim,g_model,d_model,gan_model, nb_epoch = 100, nb_batch = 50):
         
         # Import des données de profils 
-        nb_coord =  np.shape(x_train)[0]
-        nb_profil = np.shape(x_train)[1]
+        nb_coord =  np.shape(x_train)[1]
+        nb_profil = np.shape(x_train)[0]
 
         nb_batch_per_epoch = int(nb_profil/nb_batch)
         half_batch = int(nb_batch / 2)
@@ -472,7 +472,7 @@ if __name__ == "__main__":
     
     # Import des données de profils 
     x_train,y_train,nb_class,x_coord_ini = pre_process_GAN.data_GAN(Mach,Re) # Nombre de coordonnées et de profils
-    nb_coord = np.shape(x_train)[0]
+    nb_coord = np.shape(x_train)[1]
     # create the discriminator
     d_model = model.discriminateur(nb_coord, nb_class)
     # create the generator
