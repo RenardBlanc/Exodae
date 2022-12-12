b24548d9101ec85e421fbdb3efcf95caef294a03
RenardBlanc <baktacheilyas@gmail.com>
2022-12-12 13:50:14 -0500

    send to drive fonctionnel

---
+++ send_drive.py
@@ -0,0 +1,35 @@
+'''
+------Send to Drive-----
+Ce code est un programme de developpement qui nous permet 
+d'envoyer les fichiers des modèles présents dans les VM
+dans un dropbox 
+
+Created: 12/12/2022
+Updated: 12/12/2022
+@Auteur: Ilyas Baktache
+'''
+# Librairies
+import dropbox
+from dropbox import Dropbox
+import os
+
+# on se connecte au dropbox:
+# Ajoutez votre clé d'accès à Dropbox ici
+ACCESS_TOKEN = "sl.BU1JLqeBhY1sePqTZFrYwbMHvPscZPFc6huBznjAhDsuSR1JI7fCcHHVM4D90mBSangx7PZyhFAygrHZ_CTQV5Ph0_UMmhWKmyiUOSNRKOnvZVGraI-4ZFrQDChmjdaL6tdaRyaUZKWD"
+
+# Créez une instance de la classe DropboxClient en utilisant votre clé d'accès
+dbx = Dropbox(ACCESS_TOKEN,  scope=['files.content.write'])
+
+# On liste dans un premier temps les modèles présents:
+# ------------------------------------------------
+# Nom du répertoire
+directory = 'CNN/model'
+
+# Utilisez la fonction os.walk pour obtenir la liste des fichiers et des répertoires dans le répertoire
+for root, dirs, files in os.walk(directory):
+    # Affichez les fichiers dans le répertoire courant
+    for file in files:
+        # Ouvrez le fichier que vous souhaitez envoyer sur Dropbox
+        with open(file, "rb") as f:
+            # Envoyez le fichier sur Dropbox en utilisant la méthode files_upload
+            dbx.files_upload(f.read(), "/"+file, mode=dropbox.files.WriteMode.overwrite)
