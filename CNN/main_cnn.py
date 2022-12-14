263b81943cc8c28f66568712aeeb3deeb0844666
RenardBlanc <baktacheilyas@gmail.com>
2022-12-14 18:54:43 -0500

    fin cnn

--- CNN/main_cnn.py
+++ CNN/main_cnn.py
@@ -1,18 +1,83 @@
-from approx_finesse_CNN import *
+'''
+------Obtenir l'approximation de la finesse d'un profil------------
+Ce code Obtenir l'approximation de la finesse d'un profil
+Created: 23/11/2022
+Updated: 23/11/2022
+@Auteur: Ilyas Baktache
+'''
 
-if __name__ == "__main__":
-    # ----------
-    # Paramètres 
-    # ----------
-    if len(sys.argv) != 7:
-        raise Exception(
-            'Entrer <Nb_Model> <Nombre de Mach> <Nombre de Reynolds> <BATCH_SIZE> <EPOCHS> <Plot>')
+# ***********
+# Librairies
+# ***********
+
+# File managment 
+import os
+
+# Import from other class
+import sys
+sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+from precision_study import *
+
+
+
+class predict_cnn():
+
+    def interpole_profil(file_name,dir=r"data/Airfoil_Coordinate"):
+        '''
+        Cette fonction permet d'interpoler les fichier de 
+        coordonnées des profils.
+        '''
+        x_inter,y_inter = format.rawtointer(file_name, dir=dir,nb_point = 30, nb_LE = 20, nb_TE = 10,x_rang_LE = 0.15, x_rang_TE = 0.75)
+        return x_inter,y_inter
+
+    def class2fin(classe,nb_class,M,Re):
+        '''
+        Cette fonction permet de passer d'une classe 
+        à une finesse maximale avec l'incertitude résulante
+        '''
+        intervalle_finesse_max = get_ecart(M,Re,nb_class)
+
+        return intervalle_finesse_max[nb_class-1]
+
+
+    def predict_class(file_name,nb_mod,M,Re,nb_class,dir=r"data/Airfoil_Coordinate"):
+        x_inter,y_inter = predict_cnn.interpole_profil(file_name,dir=r"data/Airfoil_Coordinate")
+        model = tf.keras.models.load_model('CNN/model/' + 'mod_{}_{}_{}.h5'.format(nb_mod,M,Re))
+
+        # Fonction pour prédire la classe d'un exemple
+        if nb_mod==4:
+            prediction = model.predict([y_inter,y_inter,y_inter])
+        else : 
+            prediction = model.predict(y_inter)
+        # Récupérer la classe avec la plus grande probabilité
+        predicted_class = tf.argmax(prediction, axis=1).numpy()
+        fin_max_pred = predict_cnn.class2fin(predicted_class,nb_class,M,Re)
+        incertitude = max_ecart_pos(M,Re,nb_class,3)
+        
+        return fin_max_pred,incertitude
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        M = 0
+        Re = 50000
+        nb_class = 87
+        file_name = int(sys.argv[1]) 
+        nb_mod = int(sys.argv[2]) 
+        fin_max_pred,incertitude = predict_cnn.predict_class(file_name,nb_mod,M,Re,nb_class,dir=r"data/Airfoil_Coordinate")
+        fin_d = round(fin_max_pred+incertitude,1)
+        fin_g = round(fin_max_pred-incertitude,1)
+        print("On retrouve avec le modèle {}, une finesse entre [{},{}]".format(nb_mod,fin_g,fin_d))
+    elif len(sys.argv) == 2:
+        M = 0
+        Re = 50000
+        nb_class = 87
+        file_name = int(sys.argv[1]) 
+        fin_max_pred,incertitude = predict_cnn.predict_class(file_name,nb_mod,M,Re,nb_class,dir=r"data/Airfoil_Coordinate")
+        fin_d = round(fin_max_pred+incertitude,1)
+        fin_g = round(fin_max_pred-incertitude,1)
+        for i in range(1,5):
+            print("On retrouve avec le modèle {}, une finesse entre [{},{}]\n".format(nb_mod,fin_g,fin_d))
     else:
-        nb_model = int(sys.argv[1]) 
-        mach = int(sys.argv[2]) 
-        nb_Re = int(sys.argv[3]) 
-        batch_size = int(sys.argv[4])
-        epoch = int(sys.argv[5]) 
-        plot_bool = sys.argv[6]
-
-    models.train_model(nb_model,mach,nb_Re,BATCH_SIZE = batch_size,EPOCHS = epoch,plot=plot_bool)
+        raise Exception(
+            'Entrer <File name> <Nb_Model>')
