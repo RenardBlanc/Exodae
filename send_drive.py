3729f1fde81e3775bca42276878da86a371819b1
RenardBlanc <baktacheilyas@gmail.com>
2022-12-14 20:17:32 -0500

    fin cnn

--- send_drive.py
+++ send_drive.py
@@ -33,8 +33,9 @@ if __name__ == '__main__':
         for root, dirs, files in os.walk(directory):
             # Affichez les fichiers dans le répertoire courant
             for file in files:
+                print("fichier {}\n".format(file))
                 try :
-                    print("fichier {}\n".format(file))
+                    
                     # Ouvrez le fichier que vous souhaitez envoyer sur Dropbox
                     with open(directory + file, "rb") as f:
                         # Envoyez le fichier sur Dropbox en utilisant la méthode files_upload
